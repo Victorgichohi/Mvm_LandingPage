e348eda1e69178a282cc54da24f2f4954c286d28
victorgichohi <victorgichohi48@gmail.com>
2015-10-12 20:26:18 +0300

    custom form validation

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -6,3 +6,18 @@ class SignUpForm(forms.ModelForm):
         models =SignUp
         # this order really matters how fields are displayed
         fields=["full_name","email"]
+
+    def clean_email(self):
+        email=self.cleaned_data.get('email')
+        email_base, provider = email.split('@')
+        domain, extension = provider.split('.')
+        if not domain == 'KE':
+            raise forms.ValidationError("please provide a kenyan email")
+        if not extension == "edu":
+            raise forms.ValidationError("please use a valid edu email")
+        return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data
+        #write validation Code
+        return full_name
