c6b32e675b2377de933fd6d74eec766d44405e14
victorgichohi <victorgichohi48@gmail.com>
2015-10-12 12:41:33 +0300

    created the required signups fields and limited the output to the email,but created a class in the admin that shows the timestamps

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import SignUp
+
+# this allows us to manipulate what we see in the admin section
+class SignUpAdmin(admin.ModelAdmin):
+    list_display= ["__str__", "timestamp","updated"]
+    class Meta:
+        model = SignUp
+
+
+admin.site.register(SignUp, SignUpAdmin)
