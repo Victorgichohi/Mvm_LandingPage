ae54d3551b5a3af0a7df4c20f97366472388c099
victorgichohi <victorgichohi48@gmail.com>
2015-10-12 16:59:24 +0300

    made a new context in the views.py and rendered it then viewed at home.html

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-
-    return render(request,"home.html", {})
+    title= "My Title"
+    context={
+        "template_title":title,
+        }
+    return render(request,"home.html", context)
